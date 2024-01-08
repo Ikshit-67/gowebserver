ce8ec50ed64fb8ef1137aa2c1c38710918d6901b
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2024-01-08 19:43:51 +0530

    go web server

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintf(w, "hello !")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	// if r.Method != "POST" {
+	// 	http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	// 	return
+	// }
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request succcessfull")
+	name := r.FormValue("name")
+	fmt.Fprintf(w, "hello %s\n!", name)
+
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/form", formHandler)
+
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Server started at port: 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
